c7baf6139730184b08baadcc0b432e74ae504a8b
retroruiner <jostonfer@gmail.com>
2025-08-23 17:56:30 +0600

    added repository interfaces for User and Account

---
+++ src/main/java/ab/task/banking_system/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ab.task.banking_system.repository;
+
+import ab.task.banking_system.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+}
